3f2ce2dfc85ad6cf84bce28b86ac97b4903d6dcc
tokki.pf <tokki.pf@kakaobrain.com>
2023-08-23 00:12:06 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
       <Analytics />
     </>
   );
